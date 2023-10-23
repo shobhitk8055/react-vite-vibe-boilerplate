974fa958b649160cb81778683eb3d72a1b139622
shobhit mac <shobhitk8055@gmail.com>
2023-10-23 20:54:23 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-vite-ts-boilerplate/',
   resolve: {
     alias: {
       '@': path.resolve('./src'),
