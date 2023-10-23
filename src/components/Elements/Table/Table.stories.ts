32e3c8587aaca468d021874f115eb423ac46aa14
shobhit mac <shobhitk8055@gmail.com>
2023-10-23 19:49:43 +0530

    table with animations

--- src/components/Elements/Table/Table.stories.ts
+++ src/components/Elements/Table/Table.stories.ts
@@ -17,8 +17,6 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    variant: 'primary',
-    size: 'md',
-    children: "Primary Button"
+    pagination: true,
   },
 };
--- src/components/Elements/Table/Table.tsx
+++ src/components/Elements/Table/Table.tsx
@@ -1,27 +1,30 @@
-import { Pagination } from "@mui/material";
 import React from "react";
+import { Pagination } from "@mui/material";
 
-export const Table = () => {
+export const Table = ({ pagination = true }: { pagination: boolean }) => {
   return (
     <div>
-      <div className="d-flex justify-content-between pb-3 align-items-end">
-        <div className="d-flex align-items-center">
-          Show
-          <select
-            className="mx-2 form-control"
-            style={{ padding: "0.175rem 0.75rem" }}
-          >
-            <option selected>10</option>
-            <option>15</option>
-            <option>20</option>
-          </select>
-          entries
+      {pagination && (
+        <div className="d-flex justify-content-between pb-3 align-items-end">
+          <div className="d-flex align-items-center">
+            Show 
+            <select
+              defaultValue={10}
+              className="mx-2 form-control"
+              style={{ padding: "0.175rem 0.75rem" }}
+            >
+              <option value="10">10</option>
+              <option>15</option>
+              <option>20</option>
+            </select>
+            entries
+          </div>
+          <div className="w-25">
+            <input className="form-control" placeholder="Search" />
+          </div>
         </div>
-        <div className="w-25">
-          <input className="form-control" placeholder="Search" />
-        </div>
-      </div>
-      <table className="table ">
+      )}
+      <table className="table table-hover">
         <thead className="table-primary">
           <tr>
             <th scope="col">Sr. No.</th>
@@ -31,7 +34,7 @@ export const Table = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          <tr className="table-row">
             <th scope="row">1</th>
             <td>Mark</td>
             <td>Otto</td>
@@ -63,10 +66,17 @@ export const Table = () => {
           </tr>
         </tbody>
       </table>
-      <div className="d-flex justify-content-between align-items-end">
-        <span>Showing 1 to 10 of 1000 entries</span>
-        <Pagination color="primary" count={10} variant="outlined" shape="rounded" />
-      </div>
+      {pagination && (
+        <div className="d-flex justify-content-between align-items-end">
+          <span>Showing 1 to 10 of 1000 entries</span>
+          <Pagination
+            color="primary"
+            count={10}
+            variant="outlined"
+            shape="rounded"
+          />
+        </div>
+      )}
     </div>
   );
 };
