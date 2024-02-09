cbc0ec7a1beb9210c77f51b6fe94f6d0e87a02cd
Sarfaraz_Singh <Sarfaraz_Singh@gmail.com>
2024-02-09 18:38:56 +0530

    sample landing page

--- src/features/common/routes/index.tsx
+++ src/features/common/routes/index.tsx
@@ -1,15 +1,10 @@
 import { lazyImport } from "@/lib/lazyImport";
 
 const { Landing } = lazyImport(() => import("./Landing"), "Landing");
-const { Users } = lazyImport(() => import("./Users"), "Users");
 
 export const CommonRoutes = [
   {
     path: "",
     element: <Landing />,
-  },
-  {
-    path: "users",
-    element: <Users />,
-  },
+  }
 ];
